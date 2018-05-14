e90c36ab9f08119cee75ee95d4ae3f9387ad9fee
aco <wwsxuan@163.com>
2018-05-14 18:40:18 +0800

    添加测试

--- test/data.spec.js
+++ test/data.spec.js
@@ -9,9 +9,9 @@ let mvm = new MVM({
             }
         }
     },
-    watch: {
-        'testNum'(){
-
+    computed: {
+        doubleNum() {
+            return this.testNum * 2
         }
     }
 })
@@ -20,10 +20,14 @@ describe('base test', function () {
 
     it(' expect mvm.testNum equal 1 ', () => {
         expect(mvm.testNum).toEqual(1)
-    });
+    })
 
     it(' expect mvm.objTest.param equal param ', () => {
         expect(mvm.objTest.param).toEqual('param')
+    })
+
+    it(' expect mvm.doubleNum equal 2 ', () => {
+        expect(mvm.doubleNum).toEqual(2)
     })
 
 })
