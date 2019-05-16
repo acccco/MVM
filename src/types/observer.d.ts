534978e1d74882a3b9e49c77896769e9d57ba248
aco <wwsxuan@163.com>
2019-05-16 17:24:45 +0800

    to ts

--- src/types/observer.d.ts
+++
@@ -1,9 +0,0 @@
-import {DepInterface} from "./dep"
-import {commonObject} from "./commom"
-
-export interface ObserverInterface {
-  id: number
-  dep: DepInterface
-  walk: (obj: commonObject) => void
-  observeArray: (items: Array<any>) => void
-}
