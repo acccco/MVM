cd62415014c5e9b21973f4b649ec03bf6538667a
aco <wwsxuan@163.com>
2020-09-15 16:23:23 +0800

    clean code

--- demo-old/index.js
+++
@@ -1,8 +0,0 @@
-import RD from '../src/index'
-import jsxPlugin from './plugin/index'
-import App from './component/App'
-import './index.scss'
-
-RD.use(jsxPlugin, RD)
-
-RD.$mount(document.getElementById('app'), new RD(App))
--- src-old/index.ts
+++
@@ -1,9 +0,0 @@
-import {RD} from './core'
-import {initGlobalApi} from './core/global-api'
-
-// 初始化 RD 下的类方法
-initGlobalApi(RD)
-
-RD.version = '0.0.0'
-
-export default RD
